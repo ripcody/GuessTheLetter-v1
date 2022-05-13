66bdb46da065357a0b4a7db8c24401cc66b6a477
ripcody <ripcody@gmail.com>
2022-05-13 11:12:39 -0400

    with region

--- app.js
+++ app.js
@@ -1,64 +1,83 @@
+//Store Random Letter
 let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let rnd = parseInt(Math.random() * 26);
 let secretLetter = alphabet[rnd];
+let numGuesses = 5;
 
-let usersGuess = prompt('I am thinking of a letter. Can you guess it?');
+//
+let usersGuess = prompt('I am thinking of a letter. Can you guess it? You have ' + numGuesses + ' Guess/es.').substring(0, 1).toUpperCase();
 
-if (secretLetter == usersGuess)
+//#region First Guess
+
+if (secretLetter == usersGuess) {
   alert('Congratulations! The secret letter is ' + secretLetter);
-
+}
 else {
 
-  if (secretLetter < usersGuess)
+  if (secretLetter < usersGuess) {
     alert('The secret letter is before ' + usersGuess);
-
-  if (secretLetter > usersGuess)
+  }
+  else {
     alert('The secret letter is after ' + usersGuess);
-
-  usersGuess = prompt('Try again you have 4 guesses left');
-  if (secretLetter == usersGuess)
+  }
+  numGuesses--
+  //#endregion 
+  //#region Second Guess
+  usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
+  if (secretLetter == usersGuess) {
     alert('Congratulations! The secret letter is ' + secretLetter);
-
+  }
   else {
 
-    if (secretLetter < usersGuess)
+    if (secretLetter < usersGuess) {
       alert('The secret letter is before ' + usersGuess);
-
-    if (secretLetter > usersGuess)
+    }
+    else {
       alert('The secret letter is after ' + usersGuess);
-
-    usersGuess = prompt('Try again you have 3 guesses left');
-    if (secretLetter == usersGuess)
+    }
+    numGuesses--
+    //#endregion 
+    //#region Third Guess
+    usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
+    if (secretLetter == usersGuess) {
       alert('Congratulations! The secret letter is ' + secretLetter);
-
+    }
     else {
 
-      if (secretLetter < usersGuess)
+      if (secretLetter < usersGuess) {
         alert('The secret letter is before ' + usersGuess);
-
-      if (secretLetter > usersGuess)
+      }
+      else {
         alert('The secret letter is after ' + usersGuess);
-
-      usersGuess = prompt('Try again you have 2 guesses left');
-      if (secretLetter == usersGuess)
+      }
+      numGuesses--
+      //#endregion
+      //#region Fourth Guess
+      usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
+      if (secretLetter == usersGuess) {
         alert('Congratulations! The secret letter is ' + secretLetter);
-
+      }
       else {
 
-        if (secretLetter < usersGuess)
+        if (secretLetter < usersGuess) {
           alert('The secret letter is before ' + usersGuess);
-
-        if (secretLetter > usersGuess)
+        }
+        else {
           alert('The secret letter is after ' + usersGuess);
+        }
+        numGuesses--
+        //#endregion
+        //#region Fifth Guess
+        usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
 
-        usersGuess = prompt('Try again you have 1 guess left');
-
-        if (secretLetter == usersGuess)
+        if (secretLetter == usersGuess) {
           alert('Congratulations! The secret letter is ' + secretLetter);
-
+        }
         else
-          alert('Sorry the secret letter is ' + secretLetter);
+          alert('You are out of guesses the secret letter is ' + secretLetter);
+        //#endregion
       }
     }
   }
 }
+
