23f1be3f60a9466dfe0848e65f7ee6033e233209
ripcody <ripcody@gmail.com>
2022-05-13 11:25:57 -0400

    with loop

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-//Store Random Letter
 let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let rnd = parseInt(Math.random() * 26);
 let secretLetter = alphabet[rnd];
 let numGuesses = 5;
 
-//
-let usersGuess = prompt('I am thinking of a letter. Can you guess it? You have ' + numGuesses + ' Guess/es.').substring(0, 1).toUpperCase();
+do {
 
-//#region First Guess
+  let usersGuess = prompt('I am thinking of a letter. Can you guess it? You have ' + numGuesses + ' Guess/es.').substring(0, 1).toUpperCase();
 
-if (secretLetter == usersGuess) {
-  alert('Congratulations! The secret letter is ' + secretLetter);
-}
-else {
-
-  if (secretLetter < usersGuess) {
-    alert('The secret letter is before ' + usersGuess);
-  }
-  else {
-    alert('The secret letter is after ' + usersGuess);
-  }
-  numGuesses--
-  //#endregion 
-  //#region Second Guess
-  usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
   if (secretLetter == usersGuess) {
     alert('Congratulations! The secret letter is ' + secretLetter);
+    numGuesses = 0;
   }
+
   else {
 
     if (secretLetter < usersGuess) {
@@ -35,49 +20,12 @@ else {
     else {
       alert('The secret letter is after ' + usersGuess);
     }
-    numGuesses--
-    //#endregion 
-    //#region Third Guess
-    usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
-    if (secretLetter == usersGuess) {
-      alert('Congratulations! The secret letter is ' + secretLetter);
-    }
-    else {
 
-      if (secretLetter < usersGuess) {
-        alert('The secret letter is before ' + usersGuess);
-      }
-      else {
-        alert('The secret letter is after ' + usersGuess);
-      }
-      numGuesses--
-      //#endregion
-      //#region Fourth Guess
-      usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
-      if (secretLetter == usersGuess) {
-        alert('Congratulations! The secret letter is ' + secretLetter);
-      }
-      else {
-
-        if (secretLetter < usersGuess) {
-          alert('The secret letter is before ' + usersGuess);
-        }
-        else {
-          alert('The secret letter is after ' + usersGuess);
-        }
-        numGuesses--
-        //#endregion
-        //#region Fifth Guess
-        usersGuess = prompt('Try again you have ' + numGuesses + ' Guesses left').substring(0, 1).toUpperCase();
-
-        if (secretLetter == usersGuess) {
-          alert('Congratulations! The secret letter is ' + secretLetter);
-        }
-        else
-          alert('You are out of guesses the secret letter is ' + secretLetter);
-        //#endregion
-      }
+    numGuesses--;
+    if (numGuesses == 0) {
+      alert('You are out of guesses the secret letter is ' + secretLetter);
     }
   }
-}
+
+} while (numGuesses > 0);
 
